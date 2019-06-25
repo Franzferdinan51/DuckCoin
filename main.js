23505d97e32b85214eb53206ee79672f7fed9361
Franzferdinan51 <Franzferdinan51@gmail.com>
2019-06-25 17:19:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const SHA256 = require('crypto-js/sha256');
-class Block{
+
+class Block {
     constructor(index, timestamp, data, previousHash = ''){
         this.index = index;
         this.timestamp = timestamp;
@@ -7,35 +8,14 @@ class Block{
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
     }
-
-    calculateHash()
-    {
-        return SHA256(this.index + this.previousHash +         this.timestamp + JSON.stringify(this.data)).toString();
-    }
+calculateHash(){
+return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+  }
+ 
 }
-class Blockchain {
+
+class BlockChain {
     constructor(){
-        this.chain = [this.createGenesisBlock()];
-
+        this.chain = []
     }
 }
-createGenesisBlock()
-{
-    return new Block(0, "6/25/2019", "Genesis block", "0");
-
-}
-getLatestBlock()
-{
-return this.chain[this.chain.length -1];
-}
-addBlock(newBlock)
-{
-newBlock.previousHash = this.getLatestBlock().hash;
-newBlock.hash = newBlock.calculateHash();
-this.chain.push(newBlock);
-}
-let DuckCoin = new Blockchain();
-DuckCoin.addBlock(new Block(1, "6/25/2019", {amount: 100}));
-console.log(JSON.stringify(DuckCoin, null, 4));
-
-calculateHash()
