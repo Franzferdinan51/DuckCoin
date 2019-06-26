53bd5c82a63b858e50f319e95f62c9cb93821013
Franzferdinan51 <Franzferdinan51@gmail.com>
2019-06-26 00:36:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -80,10 +80,13 @@ isChainValid() {
 
 
 let DuckCoin = new BlockChain();
-
+console.log('Grabbing Egg 1...')
 DuckCoin.addBlock(new Block(1, "Webster & Nestle 3/27/2019", {amount: 2}));
+console.log('Grabbing Egg 2...')
 DuckCoin.addBlock(new Block(2, "Idea Origin", "4/9/2019", {amount: 1}));
+console.log('Grabbing Egg 3...')
 DuckCoin.addBlock(new Block(3, "Ducky & Ming-Ming", "4/12/2019", {amount: 2}));
+console.log('Grabbing Egg 4...')
 DuckCoin.addBlock(new Block(4, "Frankie", "6/16/2019", {amount: 1}));
 
 console.log(JSON.stringify(DuckCoin, null, 4));
