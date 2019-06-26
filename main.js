e82cc7508c82831faf3a3cf5cdc234d1d4581c00
Franzferdinan51 <Franzferdinan51@gmail.com>
2019-06-26 01:19:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,41 @@ class Transaction {
     this.amount = amount;
     this.timestamp = Date.now();
   }
-class Block {
+
+  /**
+   * Creates a SHA256 hash of the transaction
+   *
+   * @returns {string}
+   */
+  calculateHash() {
+    return SHA256(this.fromAddress + this.toAddress + this.amount + this.timestamp)
+      .toString();
+  }
+
+  /**
+   * Signs a transaction with the given signingKey (which is an Elliptic keypair
+   * object that contains a private key). The signature is then stored inside the
+   * transaction object and later stored on the blockchain.
+   *
+   * @param {string} signingKey
+   */
+  signTransaction(signingKey) {
+    // You can only send a transaction from the wallet that is linked to your
+    // key. So here we check if the fromAddress matches your publicKey
+    if (signingKey.getPublic('hex') !== this.fromAddress) {
+      throw new Error('You cannot sign transactions for other wallets!');
+    }
+    
+
+    // Calculate the hash of this transaction, sign it with the key
+    // and store it inside the transaction obect
+    const hashTx = this.calculateHash();
+    const sig = signingKey.sign(hashTx, 'base64');
+
+    this.signature = sig.toDER('hex');
+  }
+  
+  class Block {
     constructor(index, timestamp, data, previousHash = ''){
         this.index = index;
         this.transactions = transactions;
