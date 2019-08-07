fa52001c9874077d6bafa0defe45e4592c4c7755
Franzferdinan51 <Franzferdinan51@gmail.com>
2019-08-07 18:23:57 -0400

    fixxed blockchain got new error now
    ~PS C:\Users\franz\Source\Repos\DuckCoin> node main.js
    C:\Users\franz\Source\Repos\DuckCoin\main.js:56
    const createGenesisBlock{
          ^^^^^^^^^^^^^^^^^^
    
    SyntaxError: Missing initializer in const d

--- main.js
+++ main.js
@@ -2,55 +2,58 @@ const { Block } = require("./Block");
 
 const SHA256 = require('crypto-js/sha256');
 class Transaction {
-  /**
-   * @param {string} fromAddress
-   * @param {string} toAddress
-   * @param {number} amount
-   */
-  constructor(fromAddress, toAddress, amount) {
-    this.fromAddress = fromAddress;
-    this.toAddress = toAddress;
-    this.amount = amount;
-    this.timestamp = Date.now();
-  }
-
-  /**
-   * Creates a SHA256 hash of the transaction
-   *
-   * @returns {string}
-   */
-  calculateHash() {
-    return SHA256(this.fromAddress + this.toAddress + this.amount + this.timestamp).toString();
-  }
-
-  /**
-   * Signs a transaction with the given signingKey (which is an Elliptic keypair
-   * object that contains a private key). The signature is then stored inside the
-   * transaction object and later stored on the blockchain.
-   *
-   * @param {string} signingKey
-   */
-  signTransaction(signingKey) {
-    // You can only send a transaction from the wallet that is linked to your
-    // key. So here we check if the fromAddress matches your publicKey
-    if (signingKey.getPublic('hex') !== this.fromAddress) {
-      throw new Error('You cannot sign transactions for other wallets!');
+    /**
+     * @param {string} fromAddress
+     * @param {string} toAddress
+     * @param {number} amount
+     */
+    constructor(fromAddress, toAddress, amount) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+        this.timestamp = Date.now();
     }
-    
 
-    // Calculate the hash of this transaction, sign it with the key
-    // and store it inside the transaction obect
-    const hashTx = this.calculateHash();
-    const sig = signingKey.sign(hashTx, 'base64');
+    /**
+     * Creates a SHA256 hash of the transaction
+     *
+     * @returns {string}
+     */
+    calculateHash() {
+        return SHA256(this.fromAddress + this.toAddress + this.amount + this.timestamp).toString();
+    }
 
-    this.signature = sig.toDER('hex');
-  }
-class BlockChain {
-    constructor(){
+    /**
+     * Signs a transaction with the given signingKey (which is an Elliptic keypair
+     * object that contains a private key). The signature is then stored inside the
+     * transaction object and later stored on the blockchain.
+     *
+     * @param {string} signingKey
+     */
+    signTransaction(signingKey) {
+        // You can only send a transaction from the wallet that is linked to your
+        // key. So here we check if the fromAddress matches your publicKey
+        if (signingKey.getPublic('hex') !== this.fromAddress) {
+            throw new Error('You cannot sign transactions for other wallets!');
+        }
+
+
+        // Calculate the hash of this transaction, sign it with the key
+        // and store it inside the transaction obect
+        const hashTx = this.calculateHash();
+        const sig = signingKey.sign(hashTx, 'base64');
+
+        this.signature = sig.toDER('hex');
+    }
+}
+    //  class BlockChain SyntaxError: Unexpected identifier */
+  class BlockChain {
+    constructor() {
         this.chain = [this.createGenesisBlock()];
         this.difficulty = 3;
     }
-     createGenesisBlock(){
+}// styax error with const 56 and possible error on 57 (unknown fix)
+const createGenesisBlock{
       return new Block(0, "6/25/2019", "Origin Egg", "0");
     }
 
