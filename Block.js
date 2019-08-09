48f9946367487a5fd6668acb00c672e8fe3f2547
Franzferdinan51 <Franzferdinan51@gmail.com>
2019-08-09 14:21:42 -0400

    Update Block.js

--- Block.js
+++ Block.js
@@ -2,7 +2,7 @@ const SHA256 = require('crypto-js/sha256');
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
-        this.transactions = transactions;
+        this.transactions = "transactions";
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
