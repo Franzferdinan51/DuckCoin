113ceb2548fbefbe172574a50e956cebd98987a8
Franzferdinan51 <Franzferdinan51@gmail.com>
2019-08-25 22:09:26 -0400

    Update Block.js

--- Block.js
+++ Block.js
@@ -18,6 +18,27 @@ class Block {
             this.hash = this.calculateHash();
         }
         console.log(`Block mined: ${this.hash}`);
+          /**
+   * Checks if the signature is valid (transaction has not been tampered with).
+   * It uses the fromAddress as the public key.
+   *
+   * @returns {boolean}
+   */
+  isValid() {
+    // If the transaction doesn't have a from address we assume it's a
+    // mining reward and that it's valid. You could verify this in a
+    // different way (special field for instance)
+    if (this.fromAddress === null) return true;
+
+    if (!this.signature || this.signature.length === 0) {
+      throw new Error('No signature in this transaction');
+    }
+
+    const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+    return publicKey.verify(this.calculateHash(), this.signature);
+  }
+}
+
  /**
    * Validates all the transactions inside this block (signature + hash) and
    * returns true if everything checks out. False if the block is invalid.
