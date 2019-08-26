1af8c2afd017767fdf877159bb50cfd48b7c7ed9
Franzferdinan51 <Franzferdinan51@gmail.com>
2019-08-26 20:30:40 +0000

    transactions5

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const SHA256 = require('crypto-js/sha256')
 
-class Transaction2{
+class Transaction{
     constructor(fromAddress, toAddress, amount){
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
