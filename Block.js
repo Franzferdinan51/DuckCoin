38984896d71282e2a058dca725e274b5058323ab
Michael Hart <michael@mhart.dev>
2019-08-26 00:31:38 -0400

    Fixed brackets. Added missing package.json and .gitignore

--- Block.js
+++ Block.js
@@ -1,30 +1,39 @@
-const SHA256 = require('crypto-js/sha256');
+const SHA256 = require("crypto-js/sha256");
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
-        this.transactions = "transactions";
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = previousHash;
-        this.nonce = 0;
-        this.hash = this.calculateHash();
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.transactions = "transactions";
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.nonce = 0;
+    this.hash = this.calculateHash();
+  }
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data) +
+        this.nonce
+    ).toString();
+  }
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
     }
-    calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
-    }
-    mineBlock(difficulty) {
-        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
-            this.nonce++;
-            this.hash = this.calculateHash();
-        }
-        console.log(`Block mined: ${this.hash}`);}
-          /**
+    console.log(`Block mined: ${this.hash}`);
+  }
+  /**
    * Checks if the signature is valid (transaction has not been tampered with).
    * It uses the fromAddress as the public key.
    *
    * @returns {boolean}
    */
-    
+
   isValid() {
     // If the transaction doesn't have a from address we assume it's a
     // mining reward and that it's valid. You could verify this in a
@@ -32,28 +41,26 @@ class Block {
     if (this.fromAddress === null) return true;
 
     if (!this.signature || this.signature.length === 0) {
-      throw new Error('No signature in this transaction');
+      throw new Error("No signature in this transaction");
     }
 
-    const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+    const publicKey = ec.keyFromPublic(this.fromAddress, "hex");
     return publicKey.verify(this.calculateHash(), this.signature);
   }
-}
 
- /**
+  /**
    * Validates all the transactions inside this block (signature + hash) and
    * returns true if everything checks out. False if the block is invalid.
-  *
+   *
    * @returns {boolean}
    */
-    
+
   hasValidTransactions() {
     for (const tx of this.transactions) {
       if (!tx.isValid()) {
         return false;
       }
     }
-  }
     return true;
   }
 }
