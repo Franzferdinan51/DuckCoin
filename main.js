01d2ae3490f51146e11353a634c5aca007cdfb76
Franzferdinan51 <Franzferdinan51@gmail.com>
2019-08-26 16:54:49 +0000

    added validity checks

--- main.js
+++ main.js
@@ -29,6 +29,19 @@ newBlock.previousHash = this.getLatestBlock().hash;
 newBlock.hash = newBlock.calculateHash();
 this.chain.push(newBlock);
     }
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++ ){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 let DuckCoin = new Blockchain();
